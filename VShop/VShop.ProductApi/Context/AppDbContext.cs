1c0bff216a0ce13f89edfd6f657814d1ce4d9d6d
Lucas <lucasflorindoeg@gmail.com>
2023-09-05 11:11:13 -0300

    First EF migration

--- VShop/VShop.ProductApi/Context/AppDbContext.cs
+++ VShop/VShop.ProductApi/Context/AppDbContext.cs
@@ -11,4 +11,58 @@ public class AppDbContext : DbContext
     public DbSet<Category> Categories { get; set; }
     public DbSet<Product> Products { get; set; }
 
+    //fluent API
+    protected override void OnModelCreating(ModelBuilder mb)
+    {
+       //Category
+       mb.Entity<Category>().HasKey(c => c.CategoryId);
+
+        mb.Entity<Category>().
+             Property(c => c.Name).
+                 HasMaxLength(100).
+                     IsRequired();
+
+
+        //Product
+
+        mb.Entity<Product>().
+             Property(c => c.Name).
+                 HasMaxLength(100).
+                     IsRequired();
+
+        mb.Entity<Product>().
+             Property(c => c.Description).
+                 HasMaxLength(255).
+                     IsRequired();
+
+        mb.Entity<Product>().
+             Property(c => c.ImageURL).
+                 HasMaxLength(255).
+                     IsRequired();
+
+        mb.Entity<Product>().
+             Property(c => c.Price).
+                 HasPrecision(12, 2);
+
+        //Relation 1,N
+        mb.Entity<Category>()
+            .HasMany(g => g.Products)
+                .WithOne(c => c.Category)
+                    .IsRequired()
+                        .OnDelete(DeleteBehavior.Cascade);
+
+        //Hasdata method
+        mb.Entity<Category>().HasData(
+            new Category
+            {
+                CategoryId = 1,
+                Name = "Material Escolar",
+            },
+            new Category
+            {
+                CategoryId = 2,
+                Name = "Acessórios",
+            });
+    }
+
 }
